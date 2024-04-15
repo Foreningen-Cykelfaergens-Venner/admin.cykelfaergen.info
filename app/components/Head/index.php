74c4782d0b0d0860da801574fe024cfdeca907ce
Felix A. Schultz <felix.schultz@engapho.com>
2024-04-15 18:57:57 +0200

    Add favicon links to Head component and include Fonts and Scripts in index.php

--- app/components/Head/index.php
+++ app/components/Head/index.php
@@ -5,5 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include __dir__."/MetaData/index.php"; ?>
     <title><?php echo $title; ?></title>
+    <?php include __dir__."/FavIcons/index.php"; ?>
     <?php include __dir__."/Styles/index.php"; ?>
+    <?php include __dir__."/Fonts/index.php"; ?>
+    <?php include __dir__."/Scripts/index.php"; ?>
 </head>
