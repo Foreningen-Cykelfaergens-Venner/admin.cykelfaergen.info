f466c42dc53635103577b69b39de5011fff66038
Felix A. Schultz <felix.schultz@engapho.com>
2024-04-15 17:23:31 +0200

    Update header styles and language files

--- app/components/Header/index.php
+++ app/components/Header/index.php
@@ -1,6 +1,6 @@
-<header>
+<header class="main-header">
     <h1>Cykelfaegen Admin</h1>
-    <nav>
+    <nav class="navigation">
         <?php
             foreach ($nav as $key => $value) {
                 echo "<a href='$value->path'>" . $value->title . "</a>";
--- app/cykelfaergen.php
+++ app/cykelfaergen.php
@@ -1,12 +1,18 @@
 <?php
     include "functions/getDecodedContents.php";
-    $danish = getDecodedContents(__DIR__ . "/language/da.json");
-    $german = getDecodedContents( __DIR__ . "/language/de.json");
-    $english = getDecodedContents( __DIR__ .  "/language/en.json");
+    
+    $language = getDecodedContents( __DIR__ .  "/language/da.json");
+    if(isset($_COOKIE["language"]) && $_COOKIE["language"] == "da"){
+        $language = getDecodedContents(__DIR__ . "/language/da.json");
+    } else if(isset($_COOKIE["language"]) && $_COOKIE["language"] == "de"){
+        $language =  getDecodedContents( __DIR__ . "/language/de.json");
+    }else if(isset($_COOKIE["language"]) && $_COOKIE["language"] == "en"){
+        $language =  getDecodedContents( __DIR__ . "/language/en.json");
+    }
 
-    $title = $german->home->title;
-    $description = $german->home->description;
-    $nav = $german->nav;
+    $title = $language->home->title;
+    $description = $language->home->description;
+    $nav = $language->nav;
 
     include "components/Head/index.php";
     include "components/Header/index.php";
