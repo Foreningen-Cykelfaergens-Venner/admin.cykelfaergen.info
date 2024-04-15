416b96b7d58396b9f7dfc039f5e58e5679fe036d
Felix A. Schultz <felix.schultz@engapho.com>
2024-04-15 18:59:27 +0200

    Update meta description in index.php

--- app/components/Head/MetaData/index.php
+++ app/components/Head/MetaData/index.php
@@ -1,3 +1,3 @@
-<meta name="description" content="">
+<meta name="description" content="<?php echo $description;?>">
 <meta name="og:title" content="<?php echo $title;?>">
-<meta name="og:description" content="">
+<meta name="og:description" content="<?php echo $description;?>">
